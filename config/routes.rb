29494f7066cf276fc2dc1d290a33c03fba02ad1c
Jeffrey Martinez <martinezjf2@gmail.com>
2023-04-28 22:52:32 -0400

    made toggler work

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/', to: 'users#index'
+  root to: 'users#index'
   get '/videos', to: 'videos#show'
   get '/calendar', to: 'events#calendar'
 end
