dadc087fefeea72c8fa8cd63cdbbf3652ec307fb
Jeffrey Martinez <martinezjf2@gmail.com>
2023-08-01 19:08:11 -0400

    added name as the dropdown and added the logout within the options of the dropdown

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,28 @@
 class UsersController < ApplicationController
 
 
-    def index
+
+    def edit
         
     end
+
+    def update
+        
+    end
+
+    def destroy
+        
+    end
+
+
+    private
+
+    def user_params
+    end
+
+
+
+    # add email attribute to the user so if the user forgets the password a link can be sent to their email
+
 
 end
