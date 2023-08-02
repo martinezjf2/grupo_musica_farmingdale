018c4a2332e54ff1be900653de402318e7663347
Jeffrey Martinez <martinezjf2@gmail.com>
2023-08-01 22:27:03 -0400

    added onclick on the invitamos partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
 
     def show
-        
+        @current_user = current_user
     end
 
     def edit
@@ -13,24 +13,22 @@ class UsersController < ApplicationController
         @current_user = current_user
         if @current_user.update(user_params)
             redirect_to user_path(@current_user), success: "Profile Updated Successfully"
-          else
+        else
             redirect_to edit_user_path(@current_user), danger: "An error occurred while updating the profile"
-          end
+        end
     end
+
 
     def destroy
         @current_user = current_user
 
-    if @current_user.destroy
-      # Deletion successful, show success message and redirect to a relevant page
-      flash[:success] = "Your account has been deleted successfully."
-      redirect_to root_path
-    else
-      # Deletion failed, show error message and redirect back to the edit page (or any other page)
-      flash[:error] = "An error occurred while deleting your account."
-      redirect_back(fallback_location: edit_user_path(@current_user))
-    end
-        
+        if @current_user.destroy
+            flash[:success] = "Your account has been deleted successfully."
+            redirect_to root_path
+        else
+            flash[:error] = "An error occurred while deleting your account."
+            redirect_back(fallback_location: edit_user_path(@current_user))
+        end
     end
 
 
@@ -39,10 +37,6 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:username, :password, :password_confirmation, :admin_code, :first_name, :last_name, :email)
     end
-
-
-
-    # add email attribute to the user so if the user forgets the password a link can be sent to their email
 
 
 end
